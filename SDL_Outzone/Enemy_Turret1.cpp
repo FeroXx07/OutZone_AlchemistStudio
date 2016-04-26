c99eb5b72a1f7d6589303ad7d59f6c28b645b729
xavierolivenza <yotolivenza@gmail.com>
2016-04-26 21:11:21 +0200

    Turret sprite rotation fixed

--- SDL_Outzone/Enemy_Turret1.cpp
+++ SDL_Outzone/Enemy_Turret1.cpp
@@ -15,22 +15,22 @@
 
 Enemy_Turret1::Enemy_Turret1(int x, int y) : Enemy(x, y)
 {
-	turret1__0.PushBack({ 342, 54, 27, 47 });
-	turret1__22_5.PushBack({ 342, 110, 26, 46 });
-	turret1__45.PushBack({ 297, 111, 27, 45 });
-	turret1__67_5.PushBack({ 252, 113, 27, 43 });
-	turret1__90.PushBack({ 206, 113, 28, 43 });
-	turret1__112_5.PushBack({ 161, 113, 27, 44 });
-	turret1__135.PushBack({ 115, 113, 28, 44 });
-	turret1__157_5.PushBack({ 69, 113, 28, 45 });
-	turret1__180.PushBack({ 22, 113, 29, 45 });
-	turret1__202_5.PushBack({ 25, 57, 27, 45 });
-	turret1__225.PushBack({ 71, 58, 26, 44 });
-	turret1__247_5.PushBack({ 114, 57, 29, 44 });
-	turret1__270.PushBack({ 157, 58, 30, 43 });
-	turret1__292_5.PushBack({ 206, 58, 28, 43 });
-	turret1__315.PushBack({ 254, 56, 24, 45 });
-	turret1__337_5.PushBack({ 299, 55, 24, 46 });
+	turret1__0.PushBack({ 342, 54, 29, 47 });
+	turret1__22_5.PushBack({ 342, 109, 29, 47 });
+	turret1__45.PushBack({ 297, 109, 29, 47 });
+	turret1__67_5.PushBack({ 251, 109, 29, 47 });
+	turret1__90.PushBack({ 206, 109, 29, 47 });
+	turret1__112_5.PushBack({ 161, 110, 29, 47 });
+	turret1__135.PushBack({ 116, 110, 29, 47 });
+	turret1__157_5.PushBack({ 70, 111, 29, 47 });
+	turret1__180.PushBack({ 23, 111, 29, 47 });
+	turret1__202_5.PushBack({ 25, 55, 29, 47 });
+	turret1__225.PushBack({ 70, 55, 29, 47 });
+	turret1__247_5.PushBack({ 117, 54, 29, 47 });
+	turret1__270.PushBack({ 161, 54, 29, 47 });
+	turret1__292_5.PushBack({ 208, 54, 29, 47 });
+	turret1__315.PushBack({ 253, 54, 29, 47 });
+	turret1__337_5.PushBack({ 298, 54, 29, 47 });
 
 	animation = &turret1__180;
 
