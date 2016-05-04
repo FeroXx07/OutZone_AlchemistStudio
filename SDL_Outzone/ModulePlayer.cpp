514f08b9cee50d68f8b85c242c7ff00943ac2162
Sergio111 <sergallor@hotmail.com>
2016-05-04 17:43:23 +0200

    Clarified code

--- SDL_Outzone/ModulePlayer.cpp
+++ SDL_Outzone/ModulePlayer.cpp
@@ -540,8 +540,23 @@ void ModulePlayer::OnCollision(Collider* c1, Collider* c2)
 {
 	if (c1 == playercollision && destroyed == false && App->fade->IsFading() == false)
 	{
-		if (c2->type == COLLIDER_WALL || c2->type == COLLIDER_DOOR){
+		if (c2->type == COLLIDER_WALL)
+		{
 			int speed = 1;
+			int height;
+			if ((((c2->rect.y) + c2->rect.h) <= c1->rect.y + 2))
+			{
+				position = PreviousPos;
+				collisionWallT = true;
+			}
+			else if (((c2->rect.x + c2->rect.w) <= c1->rect.x + 1) || ((c1->rect.x + c1->rect.w) >= c2->rect.x + 1))
+			{
+				position = PreviousPos;
+				collisionWallS = true;
+			}
+		}
+		else if (c2->type == COLLIDER_WALL)
+		{
 			int height;
 			if ((((c2->rect.y) + c2->rect.h) <= c1->rect.y + 2))
 			{
