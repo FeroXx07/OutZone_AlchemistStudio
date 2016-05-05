560ad3bbf27d228037a2df37de41c3b15d3efaeb
xavierolivenza <yotolivenza@gmail.com>
2016-05-05 17:37:31 +0200

    Turret 2 IA preparation, don't shoot by now

--- SDL_Outzone/Enemy_Turret2.cpp
+++ SDL_Outzone/Enemy_Turret2.cpp
@@ -33,3 +33,54 @@ void Enemy_Turret2::Move()
 	position.y = original_y;
 	position.x = original_x;
 }
+
+void Enemy_Turret2::Shoot()
+{
+	unsigned int currentTime = 0;
+	bool left = false;
+	float angle = 0;
+	int i = 0;
+
+	currentTime = SDL_GetTicks();
+
+	if ((original_y >= (App->render->camera.y / 2) - RANGE) && (original_y <= (App->render->camera.y / 2) + 340 - RANGE)){
+
+		if (App->player->position.x >= original_x){
+			left = false;
+		}
+		else{
+			left = true;
+		}
+
+		angle = ((float)acos((((App->player->position.x + 14 - original_x) * 0) + ((App->player->position.y + 16 - original_y) * 1)) / (sqrt((double)((App->player->position.x + 14 - original_x)*(App->player->position.x + 14 - original_x) + (App->player->position.y + 16 - original_y)*(App->player->position.y + 16 - original_y)))*sqrt((double)(0 * 0 + 1 * 1))))) * ANGLE_CONVERT;
+		//LOG("Angle %.2f", angle);
+
+		if (currentTime > (lastTime + ENEMYSHOOTDELAY)) {
+			if (left == true){
+				if ((angle < 90) && (angle >= 0)){
+					App->particles->enemyshoot.speed.x = -ENEMY_SHOOT_SPEED * sin(angle * ANGLE_CONVERT_REVERSE);
+					App->particles->enemyshoot.speed.y = ENEMY_SHOOT_SPEED * cos(angle * ANGLE_CONVERT_REVERSE);
+				}
+				else{
+					App->particles->enemyshoot.speed.x = -ENEMY_SHOOT_SPEED * sin(angle * ANGLE_CONVERT_REVERSE);
+					App->particles->enemyshoot.speed.y = ENEMY_SHOOT_SPEED * cos(angle * ANGLE_CONVERT_REVERSE);
+				}
+				//disprar angle aleatori entre x, y (ample boca canó)
+				//App->particles->AddParticle(App->particles->enemyshoot, original_x + 8, original_y + 20, COLLIDER_ENEMY_SHOT);
+			}
+			else{
+				if ((angle < 90) && (angle >= 0)){
+					App->particles->enemyshoot.speed.x = ENEMY_SHOOT_SPEED * sin(angle * ANGLE_CONVERT_REVERSE);
+					App->particles->enemyshoot.speed.y = ENEMY_SHOOT_SPEED * cos(angle * ANGLE_CONVERT_REVERSE);
+				}
+				else{
+					App->particles->enemyshoot.speed.x = ENEMY_SHOOT_SPEED * sin(angle * ANGLE_CONVERT_REVERSE);
+					App->particles->enemyshoot.speed.y = ENEMY_SHOOT_SPEED * cos(angle * ANGLE_CONVERT_REVERSE);
+				}
+				//disprar angle aleatori entre x, y (ample boca canó)
+				//App->particles->AddParticle(App->particles->enemyshoot, original_x + 8, original_y + 20, COLLIDER_ENEMY_SHOT);
+			}
+			lastTime = currentTime;
+		}
+	}
+}
--- SDL_Outzone/Enemy_Turret2.h
+++ SDL_Outzone/Enemy_Turret2.h
@@ -15,28 +15,13 @@ private:
 	unsigned int lastTime = 0;
 
 	Animation turret2;
-/*	Animation turret1__0;
-	Animation turret1__22_5;
-	Animation turret1__45;
-	Animation turret1__67_5;
-	Animation turret1__90;
-	Animation turret1__112_5;
-	Animation turret1__135;
-	Animation turret1__157_5;
-	Animation turret1__180;
-	Animation turret1__202_5;
-	Animation turret1__225;
-	Animation turret1__247_5;
-	Animation turret1__270;
-	Animation turret1__292_5;
-	Animation turret1__315;
-	Animation turret1__337_5;*/
 
 public:
 
 	Enemy_Turret2(int x, int y);
 
 	void Move();
+	void Shoot();
 
 };
 
